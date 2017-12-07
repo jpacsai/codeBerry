8da37775ef4a20f5ea367d25aaedced841a00605
Csizmadiáné Pacsai Judit <judit.pacsai@gmail.com>
2017-12-07 20:01:23 +0100

    adding FortuneTeller.js

---
+++ JavaSrcipt/FortuneTeller.js
@@ -0,0 +1,5 @@
+function rosieTheFortuneTeller(x) {
+    var name = prompt("Hi, dear! My name is Rosie and I am an expert fortune teller. What is your name?");
+    var choice = prompt("Yes, I have seen that you would come to me, " + name ". Now please, choose one from the following and type it in the box: turtle, rose, teacup, moon.");
+    alert("Hmm... " + choice + ". Excellent choice. Let us see what your future holds.");
+}
