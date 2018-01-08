4dbed892d5aa2cb5eb67132a97013ff99ea53671
Judit Cs. Pacsai <34191321+jpacsai@users.noreply.github.com>
2018-01-08 19:40:40 +0100

    fixed code

--- JavaScript/Canvas/Advanced/Bee/Bee.js
+++ JavaScript/Canvas/Advanced/Bee/Bee.js
@@ -6,21 +6,20 @@ function bee(side, space) {
   var y = canvas.height / 3; 
   for ( var i = 1; i <= 7; i++) {
     context.beginPath();
-    context.moveTo(x+side/2,y-side);
-    context.lineTo(x+side,y);
-    context.lineTo(x+side/2,y+side);
+    context.moveTo(x+side/2, y-side);
+    context.lineTo(x+side, y);
+    context.lineTo(x+side/2, y+side);
     context.lineTo(x-side/2, y+side);
     context.lineTo(x-side, y);
     context.lineTo(x-side/2, y-side);
-    context.lineTo(x+side/2,y-side);
+    context.lineTo(x+side/2, y-side);
     context.strokeStyle = "orange";
     context.stroke();
-    y += 2 * side + space * 2;
+    y += 2*side + space*2;
     console.log(y);
     if (y+side/2 > canvas.height) {
-       // console.log("igen");
-      x = canvas.width / 2 + space;
-      y = canvas.height / 3 - side - space;
+      x = x + 1.5*side + space;
+      y = y - 5*side - 5*space;
     }
   }
   
