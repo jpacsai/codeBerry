9f2b0127bd2ac103df2f3e1061d7405c48e4754a
jpacsai <judit.pacsai@gmail.com>
2018-01-09 12:40:38 +0100

    adding variables

--- JavaScript/Canvas/Advanced/Bee/Bee.js
+++ JavaScript/Canvas/Advanced/Bee/Bee.js
@@ -1,10 +1,12 @@
 var canvas = document.getElementById('myCanvas');
 var context = canvas.getContext('2d');
 
-function bee(side, space) {
+function bee(num, side, space, color) {
+  
   var x = canvas.width/2 - 1.5 * side;
   var y = canvas.height / 3; 
-  for ( var i = 1; i <= 7; i++) {
+  
+  for ( var i = 1; i <= num; i++) {
     context.beginPath();
     context.moveTo(x+side/2, y-side);
     context.lineTo(x+side, y);
@@ -13,16 +15,21 @@ function bee(side, space) {
     context.lineTo(x-side, y);
     context.lineTo(x-side/2, y-side);
     context.lineTo(x+side/2, y-side);
-    context.strokeStyle = "orange";
+    context.strokeStyle = color;
     context.stroke();
     y += 2*side + space*2;
-    console.log(y);
+
     if (y+side/2 > canvas.height) {
       x = x + 1.5*side + space;
       y = y - 5*side - 5*space;
+    }
+    
+    if (i === num) {
+      context.fillStyle = color;
+      context.fill();
     }
   }
   
 }
 
-bee(55, 10);
+bee(7, 55, 10, "orange");
